8b90b58dd9c98abedb6df180e8040bcdeed8b0b3
Airenas Vaičiūnas <airenass@gmail.com>
2022-11-22 19:21:47 +0200

    Fix connection leak

--- internal/pkg/postgres/db.go
+++ internal/pkg/postgres/db.go
@@ -21,7 +21,7 @@ func NewDB(pool *pgxpool.Pool) (*DB, error) {
 
 // SaveRequest inserts request into DB
 func (db *DB) SaveRequest(ctx context.Context, req *persistence.ReqData) error {
-	_, err := db.pool.Query(ctx, `INSERT INTO requests(id, email, file_count, params, request_id, created) 
+	rows, err := db.pool.Query(ctx, `INSERT INTO requests(id, email, file_count, params, request_id, created) 
 	VALUES($1, $2, $3, $4, $5, $6)`, req.ID, req.Email, req.FileCount,
 		req.Params,
 		req.RequestID,
@@ -30,16 +30,18 @@ func (db *DB) SaveRequest(ctx context.Context, req *persistence.ReqData) error {
 	if err != nil {
 		return fmt.Errorf("can't insert reguest: %w", err)
 	}
+	defer rows.Close()
 	return nil
 }
 
 // SaveStatus inserts status into DB
 func (db *DB) SaveStatus(ctx context.Context, item *persistence.Status) error {
-	_, err := db.pool.Query(ctx, `INSERT INTO status(id, status, audio_ready, created) 
+	rows, err := db.pool.Query(ctx, `INSERT INTO status(id, status, audio_ready, created) 
 	VALUES($1, $2, $3, $4)`, item.ID, item.Status, item.AudioReady, item.Created)
 	if err != nil {
 		return fmt.Errorf("can't insert status: %w", err)
 	}
+	defer rows.Close()
 	return nil
 }
 
